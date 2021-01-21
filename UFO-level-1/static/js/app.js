9e756fcba764de30759c6e5894c5642283732b4d
m-rivera1 <mike.11.mxr@gmail.com>
2021-01-20 22:31:29 -0600

    added comments into app.js loop

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,17 +1,22 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE! 
+// use D3 to select the table body and create a variable for it
 var tbody = d3.select("tbody");
-console.log(data);
+// console.log(data); --just to check data
 
-// Build table
+// start of loop to get the data into the individual table rows
 data.forEach(function(ufo_sightings) {
-    console.log(ufo_sightings);
+  //console.log(ufo_sightings);  --just to check the data
+ 
+  //create variable to be used to append to each table row
     var row = tbody.append("tr");
+  //cycling through each object in the array key and value 
     Object.entries(ufo_sightings).forEach(function([key, value]) {
-        console.log(key, value);
+      //console.log(key, value); -- just to check data
+      //append table data to each table row
         var cell = row.append("td");
+      //grab the text values
         cell.text(value);
     });
 });
