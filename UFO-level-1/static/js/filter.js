f9ca9711575ec022895c2868990d3ef74dec065f
m-rivera1 <mike.11.mxr@gmail.com>
2021-01-22 22:47:26 -0600

    table filter appear correct in console still but can't get the browser table to update

--- UFO-level-1/static/js/filter.js
+++ UFO-level-1/static/js/filter.js
@@ -50,5 +50,26 @@ function runEnter() {
   var filteredData = dataTable.filter(date => date.datetime === inputValue);
 
   console.log(filteredData); 
-}
 
+  /*d3.select("tbody").text(filteredData); */
+
+//  This data copied over from app.js and modified to try to populate filtered data to the page
+  // use D3 to select the table body and create a variable for it
+   var tbody = d3.select("tbody");
+
+   filteredData.forEach(function(ufo_sightings) {
+    console.log(ufo_sightings);  //--just to check the data
+   
+    //create variable to be used to append to each table row
+      var row = tbody.append("tr");
+    //cycling through each object in the array key and value 
+      Object.entries(ufo_sightings).forEach(function([key, value]) {
+        console.log(key, value); // -- just to check data
+        //append table data to each table row
+          var cell = row.append("td");
+        //grab the text values
+          cell.text(value);
+          console.log(cell);
+        });
+    });
+}
