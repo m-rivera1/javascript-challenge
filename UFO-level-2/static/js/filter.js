d4131c8fe4fe4b64c99638ff8d2172da97d09f1f
m-rivera1 <mike.11.mxr@gmail.com>
2021-01-24 22:34:25 -0600

    tested calling function for add data to table after filter selection

--- UFO-level-2/static/js/filter.js
+++ UFO-level-2/static/js/filter.js
@@ -21,11 +21,11 @@ function runEnter() {
 // Select the input element and get the raw HTML node
   var inputDate = d3.select("#datetime");
   var inputCity = d3.select("#city");
-  // var inputState = d3.select("#state");
-  // var inputCountry = d3.select("#country");
-  // var inputShape = d3.select("#shape");
-  // var inputDuration = d3.select("#durationMinutes");
-  // var inputComments = d3.select("#comments");
+  var inputState = d3.select("#state");
+  var inputCountry = d3.select("#country");
+  var inputShape = d3.select("#shape");
+  var inputDuration = d3.select("#durationMinutes");
+  var inputComments = d3.select("#comments");
 
   //console.log(inputDate, inputCity, inputCountry, inputState, inputShape, inputDuration, inputComments);
   
@@ -33,13 +33,10 @@ function runEnter() {
   // Get the value property of the input element
   var dateValue = inputDate.property("value");
   var cityValue = inputCity.property("value");
-  var stateValue = inputState.property("value");
-  var countryValue = inputCountry.property("value");
-  var shapeValue = inputShape.property("value");
+  // var stateValue = inputState.property("value");
+  // var countryValue = inputCountry.property("value");
+  // var shapeValue = inputShape.property("value");
 
-
-
-  //console.log(dateValue, cityValue, countryValue, stateValue, shapeValue, durationVaule, commentsValue);
   
   // if-Else loop to determine which input value to filter by
 
@@ -47,29 +44,9 @@ function runEnter() {
     var filteredData = dataTable.filter(date => date.datetime === dateValue);
 
     // ****placeholder- clear table and load filtered data into table***
-    //console.log(inputDate, inputCity, inputCountry, inputState, inputShape, inputDuration, inputComments);
+    
 
-      // use D3 to select the table body and create a variable for it
-    var tbody = d3.select("tbody");
 
-   // to clear the table body before insterting sorted data or else new data will append to the end of table
-    tbody.html("");
-
-    filteredData.forEach(function(ufo_sightings) {
-      console.log(ufo_sightings);  //--just to check the data
-   
-    //create variable to be used to append to each table row
-      var row = tbody.append("tr");
-    //cycling through each object in the array key and value 
-      Object.entries(ufo_sightings).forEach(function([key, value]) {
-          console.log(value); // -- just to check data
-        //append table data to each table row
-          var cell = row.append("td");
-        //grab the text values
-          cell.text(value);
-          console.log(value);
-      });
-    });  
   }
 
   else if(cityValue) {
@@ -77,7 +54,7 @@ function runEnter() {
 
   //   // ****placeholder- clear table and load filtered data into table***
      console.log(cityValue);
-
+     fillTable(filteredData);
  }
 
   //else (inputState) {
@@ -120,8 +97,30 @@ function runEnter() {
 
   // }
   
+function fillTable()  {
+  // use D3 to select the table body and create a variable for it
+  var tbody = d3.select("tbody");
+  // to clear the table body before insterting sorted data or else new data will append to the end of table
+  tbody.html("");
+     
+  filteredData.forEach(function(ufo_sightings) {
+  console.log(ufo_sightings);  //--just to check the data
+         
+  //create variable to be used to append to each table row
+  var row = tbody.append("tr");
+  //cycling through each object in the array key and value 
+  Object.entries(ufo_sightings).forEach(function([key, value]) {
+  console.log(value); // -- just to check data
+  //append table data to each table row
+  var cell = row.append("td");
+  //grab the text values
+  cell.text(value);
+  console.log(value);
+   });
+  });    
+}
 
-  
+ 
 
 
   // // use D3 to select the table body and create a variable for it
