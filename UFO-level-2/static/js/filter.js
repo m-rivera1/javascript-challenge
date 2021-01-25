b5bb30dc527e917f59da9e3c969a1c61d5772641
m-rivera1 <mike.11.mxr@gmail.com>
2021-01-24 18:25:27 -0600

    added search boxes for all column headings and added inital if-else for filters

--- UFO-level-2/static/js/filter.js
+++ UFO-level-2/static/js/filter.js
@@ -18,18 +18,87 @@ function runEnter() {
   // Prevent the page from refreshing
   d3.event.preventDefault();
   
-  // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
+// Select the input element and get the raw HTML node
+  var inputDate = d3.select("#datetime");
+  var inputCity = d3.select("#city");
+  var inputState = d3.select("#state");
+  var inputCountry = d3.select("#country");
+  var inputShape = d3.select("#shape");
+  var inputDuration = d3.select("#durationMinutes");
+  var inputComments = d3.select("#comments");
 
   // Get the value property of the input element
-  var inputValue = inputElement.property("value");
+  var inputDate = inputElement.property("value");
+  var inputCity = inputElement.property("value");
+  var inputState = inputElement.property("value");
+  var inputCountry = inputElement.property("value");
+  var inputShape = inputElement.property("value");
+  var inputDuration = inputElement.property("value");
+  var inputComments = inputElement.property("value");
 
-  console.log(inputValue);
-  console.log(dataTable);
 
-  var filteredData = dataTable.filter(date => date.datetime === inputValue);
+  console.log(inputDate, inputCity, inputCountry, inputState, inputShape, inputDuration, inputComments);
+  
+  // if-Else loop to determine which input value to filter by
 
-  console.log(filteredData); 
+  if(inputDate) {
+    var filteredData = dataTable.filter(date => date.datetime === inputDate);
+
+    // ****placeholder- clear table and load filtered data into table***
+    console.log(inputDate, inputCity, inputCountry, inputState, inputShape, inputDuration, inputComments);
+
+  }
+
+  else if(inputCity) {
+    var filteredData = dataTable.filter(city => city.city === inputCity);
+
+    // ****placeholder- clear table and load filtered data into table***
+    console.log(inputDate, inputCity, inputCountry, inputState, inputShape, inputDuration, inputComments);
+
+  }
+
+  else if(inputState) {
+    var filteredData = dataTable.filter(state => state.state === inputState);
+
+    // ****placeholder- clear table and load filtered data into table***
+    console.log(inputDate, inputCity, inputCountry, inputState, inputShape, inputDuration, inputComments);
+
+  }
+  
+  else if(inputCountry) {
+    var filteredData = dataTable.filter(country => country.country === inputCountry);
+
+    // ****placeholder- clear table and load filtered data into table***
+    console.log(inputDate, inputCity, inputCountry, inputState, inputShape, inputDuration, inputComments);
+
+  }
+
+  else if(inputShape) {
+    var filteredData = dataTable.filter(shape => shape.shape === inputShape);
+
+    // ****placeholder- clear table and load filtered data into table***
+    console.log(inputDate, inputCity, inputCountry, inputState, inputShape, inputDuration, inputComments);
+
+  }
+
+  else if(inputDuration) {
+    var filteredData = dataTable.filter(duration => duration.duration === inputDuration);
+
+    // ****placeholder- clear table and load filtered data into table***
+    console.log(inputDate, inputCity, inputCountry, inputState, inputShape, inputDuration, inputComments);
+
+  }
+  
+  else if(inputComments) {
+    var filteredData = dataTable.filter(comments => comments.comments === inputComments);
+
+    // ****placeholder- clear table and load filtered data into table***
+    console.log(inputDate, inputCity, inputCountry, inputState, inputShape, inputDuration, inputComments);
+
+  }
+  
+
+  
 
 
   // use D3 to select the table body and create a variable for it
